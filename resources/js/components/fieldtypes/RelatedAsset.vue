df342d02bee345a802517474919e1e68e1310be2
Jack Sleight <jacksleight@gmail.com>
2023-12-08 17:47:07 +0000

    Add assets support

---
+++ resources/js/components/fieldtypes/RelatedAsset.vue
@@ -0,0 +1,33 @@
+<template>
+    
+    <div class="item select-none">
+        <div class="item-inner">
+            <div
+                v-if="asset.is_svg"
+                class="img svg-img h-7 w-7 bg-no-repeat bg-center bg-cover text-center flex items-center justify-center"
+                :style="'background-image:url(' + asset.thumbnail + ')'"
+            ></div>
+            <img
+                class="asset-thumbnail max-h-full max-w-full rounded w-7 h-7 object-cover"
+                loading="lazy"
+                :src="asset.thumbnail"
+                :alt="asset.basename"
+                v-if="asset.is_image"
+            />
+            <file-icon :extension="asset.extension" v-else class="w-7 h-7" />
+            <a :href="asset.edit_url" v-text="asset.title" class="flex items-center flex-1 ml-3 text-xs text-left truncate w-full" v-tooltip="asset.title" />
+            <div v-text="asset.size" class="hidden @xs:inline asset-filesize text-xs text-gray-600 px-2" />
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+    props: {
+        asset: Object,
+    },
+
+};
+</script>
--- resources/js/components/fieldtypes/RelatedItem.vue
+++ resources/js/components/fieldtypes/RelatedItem.vue
@@ -2,7 +2,7 @@
 
     <div class="item select-none">
         <div class="item-inner">
-            <div v-if="statusIcon" class="little-dot mr-2 hidden@sm:block" :class="item.status" />
+            <div class="little-dot mr-2 hidden@sm:block" :class="item.status" />
             <a :href="item.edit_url" v-text="item.title" class="truncate" v-tooltip="item.title" />
             <div class="flex items-center flex-1 justify-end">
                 <div v-if="item.collection" v-text="item.collection.title" class="text-4xs text-gray-600 uppercase whitespace-nowrap mr-2 hidden @sm:block" />
@@ -17,7 +17,6 @@ export default {
 
     props: {
         item: Object,
-        statusIcon: Boolean,
     },
 
 }
